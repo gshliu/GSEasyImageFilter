e1db5e653264cc22c341f1d7c3056dfd18f69df4
liuguangsheng <liuguangsheng@huxiu.com>
2015-11-12 16:16:24 +0800

    Initial Commit

---
+++ GSEasyImageFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSEasyImageFilter
+//
+//  Created by liuguangsheng on 15/11/12.
+//  Copyright © 2015年 liugs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GSEasyImageFilter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GSEasyImageFilter
+//
+//  Created by liuguangsheng on 15/11/12.
+//  Copyright © 2015年 liugs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
